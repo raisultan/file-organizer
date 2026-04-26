02bc905201bca3a1f208821b057d0c3334c9f332
raisultan <ki.xbozz@gmail.com>
2026-04-26 23:31:38 +0500

    stage 2: правила сортировки

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module file-organizer
 
-go 1.23
+go 1.22
--- main.go
+++ main.go
@@ -1,4 +1,25 @@
 package main
 
+import "fmt"
+
+var DefaultRules = map[string]string{
+	".jpg":  "Images",
+	".jpeg": "Images",
+	".png":  "Images",
+	".pdf":  "Documents",
+	".doc":  "Documents",
+	".docx": "Documents",
+	".txt":  "Documents",
+	".mp3":  "Music",
+	".wav":  "Music",
+	".mp4":  "Video",
+	".avi":  "Video",
+	".zip":  "Archives",
+	".rar":  "Archives",
+}
+
 func main() {
+	for ext, category := range DefaultRules {
+		fmt.Printf("%s -> %s\n", ext, category)
+	}
 }
